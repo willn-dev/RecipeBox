b6d96e89aa72d134a56815bccc8ac8567f011630
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-05-11 23:39:45 -0500

    prepped schema irl, and then added daisyUI for tailwindCSS, and got npm init and finished directory layout basics

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #------------------------------------------------------------------------------------------------------
 app = Flask(__name__)
@@ -6,5 +6,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template('templates/index.html')
+    return render_template('index.html')
 
