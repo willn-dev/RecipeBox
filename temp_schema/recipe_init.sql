4662cb496b33b99e4f5021344a054717df068701
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-05-12 22:30:42 -0500

    planned and designed schema, ready for first launch test with Postgres

---
+++ temp_schema/recipe_init.sql
@@ -0,0 +1,22 @@
+CREATE TYPE units AS ENUM ('tsp', 'Tbsp', 'C', 'pt', 'qt', 'gal', 'g', 'kg', 'mg', 'oz', 'fl_oz');
+CREATE TYPE preparations AS ENUM ('fresh', 'frozen', 'grated', 'chopped', 'sliced', 'diced', 'ground', 'minced', 'dried', 'raw', 'whole');
+
+CREATE TABLE IF NOT EXISTS recipes(
+    recipe_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255),
+    instructions TEXT
+);
+
+CREATE TABLE IF NOT EXISTS ingredients(
+    ingredient_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS recipes_ingredients(
+    recipe_id INT REFERENCES recipes(recipe_id),
+    ingredient_id INT REFERENCES ingredients(ingredient_id),
+    quantity NUMERIC NULL,
+    unit units NULL,
+    preparation preparations NULL
+);
+
