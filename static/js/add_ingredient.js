11f6145e1081bf0bb106a06ef71adb338b5963e6
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-05-22 23:20:06 -0500

    js for adding and removing multiple ingredients, and aligned removal button with absolute placement.

--- static/js/add_ingredient.js
+++ static/js/add_ingredient.js
@@ -1,2 +1,31 @@
 // for adding another input field to the ingredients list.
 
+let count_ingredients = 1;
+
+function button_listen(){
+    let button = document.querySelector('#add-new-btn');
+
+    button.addEventListener('click', addIngredient);
+}
+
+function addIngredient(){
+    let ingredientContainer = document.querySelector('#ingredient-col');
+    let button = document.querySelector('#add-new-btn');
+    let newRow = document.createElement('div');
+
+    newRow.classList.add('flex', 'flex-row', 'gap-1', 'items-center', 'relative');
+
+    newRow.innerHTML = `
+    <button class="btn md:btn-xs btn-ghost align-center absolute -left-11 md:-left-6" id="removeIngredient" onclick="this.parentElement.remove()">&times;</button>
+    <input name = "ingedient_${count_ingredients}" type="text" required class= "input input-lg input-accent w-full text-lg font-mono">
+    <input placeholder="Qty." type="number" name="ingredient_qty_${count_ingredients}" class="input validator input-lg input-accent text-lg font-mono w-1/4"> 
+    `;
+
+    newRow.classList.add('mt-2'); 
+    
+    ingredientContainer.insertBefore(newRow, button);
+    count_ingredients++;
+
+}
+
+document.addEventListener('DOMContentLoaded', button_listen);
