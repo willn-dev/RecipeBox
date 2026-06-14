ffbbb6acfe7206d17dc67abf0931af2da7ccb159
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-06-14 08:46:05 -0500

    aligned plan screen, set up json const being passed to js script for loading the list dynamically.

--- static/js/planner.js
+++ static/js/planner.js
@@ -14,7 +14,9 @@ function populate_recipe(){
 
 }
 
-
+//need to use json_pool const
+//also need to take note of the amount selected on slider with days_needed
+//solve when refresh selects new from pool.
 
 document.addEventListener('DOMContentLoaded', hide_btn);
 document.addEventListener('DOMContentLoaded', populate_recipe);
